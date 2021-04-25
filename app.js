651a968ab0f09af4ef3b99e27d9fe9ff65a5d794
Paulo Roberto <prmorais1302@gmail.com>
2021-04-24 23:04:28 -0300

    Adicionando primeira rota(router)

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { config } from 'dotenv';
 
+import userRouter from './routes/user.routes';
+
+// Configuração do dotenv
 config();
 
 const app = express();
 
+// Conexão com o bando de dados
 mongoose
   .connect(process.env.MONGO_URL, {
     useCreateIndex: true,
@@ -15,8 +19,12 @@ mongoose
   .then(() => console.log('BD conectado com sucesso!'))
   .catch((err) => console.log('Erro ao conectar BD', err));
 
+// Middlewares de rotas
+app.use('/api', userRouter);
+
 const port = process.env.PORT || 8000;
 
+// Inicialização do servidor
 app.listen(port, () =>
   console.log('Servidor rodando em http://localhost:', port)
 );
