17fe1deee36b7758d740ba99edaf1a0149e2e5b0
Paulo Roberto <prmorais1302@gmail.com>
2021-04-25 11:20:04 -0300

    Adicionando UserModel

---
+++ models/user.model.js
@@ -0,0 +1,70 @@
+import { Schema, model } from 'mongoose';
+import crypto from 'crypto';
+import uuidv1 from 'uuid/dist/v1';
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 32,
+      minLength: 6,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      trim: true,
+    },
+    salt: {
+      type: String,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
+    history: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps }
+);
+
+// Virtual Schema
+userSchema
+  .virtual('password')
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.methods = {
+  encryptPassword: function (password) {
+    if (!password) return '';
+
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+    } catch (err) {
+      return '';
+    }
+  },
+};
+
+export default model('User', userSchema);
