0aa7200aea04ad9856e41f0173e90417fe72db23
Paulo Roberto <prmorais1302@gmail.com>
2021-04-26 20:02:45 -0300

    Implementando midleware productById e busca de um produto

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -5,6 +5,25 @@ import fs from 'fs';
 import { errorHandler } from '../helpers/dbErrorHandler';
 import Product from '../models/product.model';
 
+const productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err || !product) {
+      return res.status(404).json({
+        error: 'Produto não cadastrado!',
+      });
+    }
+
+    req.product = product;
+    next();
+  });
+};
+
+const read = (req, res) => {
+  console.log(req.product);
+  req.product.photo = undefined;
+  res.json(req.product);
+};
+
 const create = (req, res) => {
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
@@ -18,6 +37,27 @@ const create = (req, res) => {
     let product = new Product(fields);
 
     if (files.photo) {
+      if (files.photo.size > 2000000) {
+        return res.status(400).json({
+          error: 'Imagem deve ter até 2 MB de tamanho!',
+        });
+      }
+
+      const { name, description, price, category, quantity, shipping } = fields;
+
+      if (
+        !name ||
+        !description ||
+        !price ||
+        !category ||
+        !quantity ||
+        !shipping
+      ) {
+        return res.status(400).json({
+          error: 'Todos os campos devem ser informados',
+        });
+      }
+
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
@@ -34,4 +74,4 @@ const create = (req, res) => {
   });
 };
 
-export { create };
+export { create, productById, read };
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import { isAuth, isAdmin, requireSignin } from '../controllers/auth.controller';
 import { userById } from '../controllers/user.controller';
-import { create } from '../controllers/product.controller';
+import { create, productById, read } from '../controllers/product.controller';
 
 const router = Router();
 
+router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
 // router.post("/signin", signin);
-// router.get("/signout", signout);
 
 router.param('userId', userById);
+router.param('productId', productById);
 
 export default router;
