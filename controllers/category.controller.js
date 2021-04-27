a60c308a153126dd61172fd7885b2b2ce85f8c18
Paulo Roberto <prmorais1302@gmail.com>
2021-04-26 22:24:09 -0300

    Implementando readAll, update e remove de category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,10 +1,22 @@
+import lodash from 'lodash';
+
 import { errorHandler } from '../helpers/dbErrorHandler';
 import Category from '../models/category.model';
 
+const readAll = (req, res) => {
+  Category.find().exec((err, categories) => {
+    if (err || !categories) {
+      return res.status(404).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(categories);
+  });
+};
+
 const categoryById = (req, res, next, id) => {
-  console.log(id);
   Category.findById(id).exec((err, category) => {
-    if (err) {
+    if (err || !category) {
       return res.status(404).json({
         error: 'Categoria não cadastrada!',
       });
@@ -17,6 +29,36 @@ const categoryById = (req, res, next, id) => {
 
 const read = (req, res) => {
   res.json(req.category);
+};
+
+const remove = (req, res) => {
+  let category = req.category;
+
+  category.remove((err, deletedCategory) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json({
+      message: `${deletedCategory.name} removido com sucesso!`,
+    });
+  });
+};
+
+const update = (req, res) => {
+  let category = req.category;
+  category = lodash.extend(category, req.body);
+
+  category.save((err, category) => {
+    if (err || !category) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+
+    res.json(category);
+  });
 };
 
 const create = (req, res) => {
@@ -33,4 +75,4 @@ const create = (req, res) => {
   });
 };
 
-export { create, categoryById, read };
+export { categoryById, read, readAll, remove, update, create };
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -2,18 +2,35 @@ import { Router } from 'express';
 
 import { isAdmin, isAuth, requireSignin } from '../controllers/auth.controller';
 import { userById } from '../controllers/user.controller';
-import { create, read, categoryById } from '../controllers/category.controller';
+import {
+  create,
+  read,
+  readAll,
+  categoryById,
+  update,
+  remove,
+} from '../controllers/category.controller';
 
 const router = Router();
 
+router.get('/categories', readAll);
 router.get('/category/:categoryId', read);
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+
 router.put(
   '/category/:categoryId/:userId',
   requireSignin,
   isAuth,
   isAdmin,
-  create
+  update
+);
+
+router.delete(
+  '/category/:categoryId/:userId',
+  requireSignin,
+  isAuth,
+  isAdmin,
+  remove
 );
 
 router.param('userId', userById);
